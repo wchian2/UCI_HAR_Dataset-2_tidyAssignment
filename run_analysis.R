0faf339eb4b955f5e33667111e90f9a36546603c
wchian2 <william.pharmd@gmail.com>
2015-12-12 17:10:58 -0600

    this is the first version, hope it makes it synchronizes with my GitHub <fingers crossed>

---
+++ run_analysis.R
@@ -0,0 +1,16 @@
+# Load dplyr library
+library(dplyr)
+
+# Read and combine x, y, and subject data
+
+x_train <- read.table("./UCI HAR Dataset-2/train/X_train.txt", header=FALSE)
+x_test <- read.table("./UCI HAR Dataset-2/test/X_test.txt", header=FALSE)
+x_data <- rbind(x_train, x_test)
+
+y_train <- read.table("./UCI HAR Dataset-2/train/y_train.txt", header=FALSE)
+y_test <- read.table("./UCI HAR Dataset-2/test/y_test.txt", header=FALSE)
+y_data <- rbind(y_train, y_test)
+
+subject_train <- read.table("./UCI HAR Dataset-2/train/subject_train.txt", header=FALSE)
+subject_test <- read.table("./UCI HAR Dataset-2/test/subject_test.txt", header=FALSE)
+subject_data <- rbind(subject_train, subject_test)
